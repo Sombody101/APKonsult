4fd6ec83a97af8eef4210b87a51ca0368531d4be
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-06-06 01:24:52 -0600

    Update text template file resolution in GitHub actions

--- APKonsult/Properties/AssemblyInfo.cs
+++ APKonsult/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
-/* Generated on Friday, June 06, 2025 01:06:33 AM */
-[assembly: AssemblyVersion("2025.06.06.0106")]
-[assembly: AssemblyFileVersion("2025.06.06.0106")]
+/* Generated on Friday, June 06, 2025 01:20:49 AM */
+[assembly: AssemblyVersion("2025.06.06.0120")]
+[assembly: AssemblyFileVersion("2025.06.06.0120")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("DEBUG+2025.06.06.0106 RC1.4")]
+[assembly: AssemblyInformationalVersion("DEBUG+2025.06.06.0120 RC1.4")]
 #else
-[assembly: AssemblyInformationalVersion("RELEASE+2025.06.06.0106 RC1.4")]
+[assembly: AssemblyInformationalVersion("RELEASE+2025.06.06.0120 RC1.4")]
 #endif
