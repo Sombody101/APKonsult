357265195570a248f835f80cf9e297b537bd8b53
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-06-06 01:12:59 -0600

    Added bot version updater

---
+++ APKonsult/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+/* Generated on Friday, June 06, 2025 01:06:33 AM */
+[assembly: AssemblyVersion("2025.06.06.0106")]
+[assembly: AssemblyFileVersion("2025.06.06.0106")]
+#if DEBUG
+[assembly: AssemblyInformationalVersion("DEBUG+2025.06.06.0106 RC1.4")]
+#else
+[assembly: AssemblyInformationalVersion("RELEASE+2025.06.06.0106 RC1.4")]
+#endif
