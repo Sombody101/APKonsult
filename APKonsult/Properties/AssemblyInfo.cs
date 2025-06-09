455b08097412f16b78447544b6b660424d8ec7d9
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-06-09 00:16:44 -0600

    Bump

--- APKonsult/Properties/AssemblyInfo.cs
+++ APKonsult/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
-/* Generated on Friday, June 06, 2025 01:20:49 AM */
-[assembly: AssemblyVersion("2025.06.06.0120")]
-[assembly: AssemblyFileVersion("2025.06.06.0120")]
+/* Generated on Monday, June 09, 2025 12:00:42 AM */
+[assembly: AssemblyVersion("2025.06.09.0000")]
+[assembly: AssemblyFileVersion("2025.06.09.0000")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("DEBUG+2025.06.06.0120 RC1.4")]
+[assembly: AssemblyInformationalVersion("DEBUG+2025.06.09.0000 RC1.4")]
 #else
-[assembly: AssemblyInformationalVersion("RELEASE+2025.06.06.0120 RC1.4")]
+[assembly: AssemblyInformationalVersion("RELEASE+2025.06.09.0000 RC1.4")]
 #endif
