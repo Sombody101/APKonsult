ab1193135ae58fa076de6e0c2b5040506e2bb15c
Sombody101 <84810042+Sombody101@users.noreply.github.com>
2025-06-23 17:02:31 -0600

    Wrong API url + bump

--- APKonsult/Properties/AssemblyInfo.cs
+++ APKonsult/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
-/* Generated on Monday, June 23, 2025 04:55:06 PM */
-[assembly: AssemblyVersion("2025.06.23.1655")]
-[assembly: AssemblyFileVersion("2025.06.23.1655")]
+/* Generated on Monday, June 23, 2025 05:02:19 PM */
+[assembly: AssemblyVersion("2025.06.23.1702")]
+[assembly: AssemblyFileVersion("2025.06.23.1702")]
 #if DEBUG
-[assembly: AssemblyInformationalVersion("DEBUG+2025.06.23.1655 RC1.4")]
+[assembly: AssemblyInformationalVersion("DEBUG+2025.06.23.1702 RC1.4")]
 #else
-[assembly: AssemblyInformationalVersion("RELEASE+2025.06.23.1655 RC1.4")]
+[assembly: AssemblyInformationalVersion("RELEASE+2025.06.23.1702 RC1.4")]
 #endif
